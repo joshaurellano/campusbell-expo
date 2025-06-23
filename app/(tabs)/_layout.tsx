fd1a16337b85cbfc98c73eed54bfea575841ec65
josh <jaaurellano@gbox.ncf.edu.ph>
2025-06-23 14:41:36 +0800

    Added icons to navigation tabs and update background color

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,18 +1,54 @@
+import Ionicons from '@expo/vector-icons/Ionicons';
 import { Tabs } from 'expo-router';
 
 export default function TabLayout () {
   return (
-    <Tabs>
-      <Tabs.Screen name='home'
+    <Tabs  screenOptions={{
+    headerStyle: {
+      backgroundColor: '#25292e',
+    },
+    headerShadowVisible: false,
+    headerTintColor: '#fff',
+    tabBarStyle: {
+      backgroundColor: '#25292e',
+    },
+  }}>
+      <Tabs.Screen 
+      name='home'
       options={{
-        title: "Home"
+        title: "Home",
+        tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
+          ),
       }}
        />
-        <Tabs.Screen name='notification' />
+        <Tabs.Screen 
+        name='notification'
+        options={{
+        title: "Notifications",
+        tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'notifications' : 'notifications-outline'} color={color} size={24} />
+          ),
+      }} />
 
-         <Tabs.Screen name='message' />
+        <Tabs.Screen 
+        name='message'
+        options={{
+        title: "Message",
+        tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'chatbubble-ellipses' : 'chatbubble-ellipses-outline'} color={color} size={24} />
+          ),
+      }} />
 
-        <Tabs.Screen name='profile' />
+        <Tabs.Screen 
+        name='profile' 
+        options={{
+        title: "Profile",
+        tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'person-circle' : 'person-circle-outline'} color={color} size={24} />
+          ),
+      }}
+        />
     </Tabs>
   )
 }
