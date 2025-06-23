5c8b1fc35d31791ba97feee446059aa1dfa947a7
josh <jaaurellano@gbox.ncf.edu.ph>
2025-06-23 14:20:59 +0800

    added logout function

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,9 +1,19 @@
-import { StyleSheet, Text, View } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useRouter } from 'expo-router';
+import { Button, StyleSheet, View } from "react-native";
 
 export default function profile() {
+const apiUrl = process.env.EXPO_PUBLIC_API_URL;
+const router = useRouter();
+
+  const handleLogout = async () => {
+    const logout = await AsyncStorage.removeItem('token');
+    router.replace('/')
+  }
+  
   return (
     <View style={styles.container}>
-      <Text>Profile</Text>
+      <Button title="logout" onPress={handleLogout}/>
     </View>
   );
 }
